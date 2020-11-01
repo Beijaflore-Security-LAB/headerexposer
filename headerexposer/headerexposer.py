c03e824fb2bd1fabd9061653fae7576d0311ca7c
Alexandre Janvrin <alexandre.janvrin@reseau.eseo.fr>
2020-11-01 15:58:21 +0100

    big refactoring

--- headerexposer/headerexposer.py
+++ headerexposer/headerexposer.py
@@ -11,12 +11,10 @@ from tabulate import tabulate
 from shutil import get_terminal_size
 from time import gmtime, strftime
 from datetime import timedelta
-import json
+from json import loads as json_loads
 import re
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
-WIDTH = get_terminal_size().columns // 80 * 80
 
 RATINGS = {
         "good":   "\033[92m[ＧＯＯＤ]\033[0m",
@@ -42,22 +40,112 @@ def colorize(text, color):
 def print_color(text, color):
     print(colorize(text, color))
 
+def tabulate_dict(dict, max_width=None):
+
+    if max_width is None:
+        max_width = get_terminal_size().columns
+
+    max_dict_key_len = 0
+    max_dict_value_len = 0
+    for key, value in dict.items():
+
+        try:
+            value_len = len(value)
+        except TypeError:
+            value_len = 4
+        
+        max_dict_value_len = max(max_dict_value_len, value_len)
+        max_dict_key_len = max(max_dict_key_len, len(key))
+
+    max_v_width = max_width - max_dict_key_len - 3
+    
+    table = [[str(k), "\\\n".join(wrap(
+        str(v), width=max_v_width
+        ))] for k, v in dict.items()]
+
+    return tabulate(table)
+
+def tabulate_findings(findings, max_width=None):
+
+    if max_width is None:
+        max_width = get_terminal_size().columns
+
+    max_header_name_len = 0
+    max_header_value_len = 0
+    max_rating_len = 0
+    max_reference_len = 0
+
+    for finding in findings:
+        
+        try:
+            value_len = len(finding["value"])
+        except TypeError:
+            value_len = 4
+
+        max_header_name_len = max(max_header_name_len, len(finding["header"]))
+        max_header_value_len = max(max_header_value_len, value_len)
+        max_rating_len = max(max_rating_len, len(RATINGS[finding["rating"]]))
+        
+        for ref in finding["references"]:
+            max_reference_len = max(max_reference_len, len(ref))
+
+    columns_width = (max_width - max_header_name_len - max_rating_len - 7)
+
+    if max_header_value_len <= columns_width // 2:
+        e_width = columns_width - max_header_value_len - 1
+        v_width = max_header_value_len + 1
+    else:
+        v_width = columns_width // 2
+        e_width = columns_width // 2
+
+    if max_reference_len > e_width and max_reference_len < columns_width - 10:
+        e_width = max_reference_len
+        v_width = columns_width - e_width
+
+    findings_table = []
+
+    for finding in findings:
+
+        if finding["value"] is None:
+            value = "Absent"
+        else:
+            value = "\\\n".join(wrap(finding["value"], width=v_width))
+        
+        rating = RATINGS.get(finding["rating"].lower())
+
+        paragraphs = " ".join(finding["explanations"]).splitlines()
+        lines = ["\n".join(wrap(p, e_width)) for p in paragraphs]
+        explanation = "\n".join(lines)
+        
+        references = finding["references"]
+        if references != []:
+            lines = ["\n".join(wrap(r, e_width)) for r in references]
+            ref_lines = "\n".join(lines).splitlines()
+            explanation += "\nReferences:\n\033[4;94m"
+            explanation += "\033[0m\n\033[4;94m".join(ref_lines)
+            explanation += "\033[0m"
+
+        findings_table += [[finding["header"], value, rating, explanation]]
+
+    table_headers = ["Header", "Value", "Rating", "Explanation"]
+    return tabulate(findings_table, headers=table_headers)
+
 def build_request_arguments(
         url,
-        method="GET",
-        params=None,
-        timeout=None,
-        disallow_redirects=False,
-        proxy=None,
-        verify=False,
-        cert=None,
-        data=None,
-        file=None,
-        headers=None,
-        user_agent=None,
-        cookies=None,
-        username=None,
-        password=None
+        method             = "GET",
+        params             = None,
+        timeout            = None,
+        disallow_redirects = False,
+        proxy              = None,
+        verify             = False,
+        cert               = None,
+        data               = None,
+        file               = None,
+        headers            = None,
+        user_agent         = None,
+        cookies            = None,
+        username           = None,
+        password           = None
         ):
 
     if data is not None and file is not None:
@@ -66,18 +154,18 @@ def build_request_arguments(
         exit(1)
 
     request_arguments = {
-            "method": method,
-            "url": url,
-            "params": None,
-            "data": None,
-            "headers": {},
-            "cookies": None,
-            "auth": None,
-            "timeout": timeout,
+            "method"         : method,
+            "url"            : url,
+            "params"         : None,
+            "data"           : None,
+            "headers"        : {},
+            "cookies"        : None,
+            "auth"           : None,
+            "timeout"        : timeout,
             "allow_redirects": not disallow_redirects,
-            "proxies": {"http": proxy, "https": proxy},
-            "verify": verify,
-            "cert": cert
+            "proxies"        : {"http": proxy, "https": proxy},
+            "verify"         : verify,
+            "cert"           : cert
         }
 
     if params is not None:
@@ -147,6 +235,84 @@ def build_request_arguments(
 
     return request_arguments
 
+def load_baseline(baseline_path):
+    with open(baseline_path, "rb") as f:
+        baseline_json = f.read()
+        baseline_json = baseline_json.replace(b"[green]", b"\u001b[92m")
+        baseline_json = baseline_json.replace(b"[yellow]", b"\u001b[93m")
+        baseline_json = baseline_json.replace(b"[red]", b"\u001b[91m")
+        baseline_json = baseline_json.replace(b"[normal]", b"\u001b[0m")
+        return json_loads(baseline_json)
+
+def analyse_headers(headers, baseline, short=False):
+
+    findings = []
+
+    for b_header in baseline["headers"]:
+
+        header_name = b_header["name"]
+        header_value = headers.get(header_name)
+        explanations = []
+
+        if not short and b_header.get("description") is not None:
+            explanations += [b_header["description"]]
+
+        if header_value is None:
+            explanations += [b_header["absent_explanation"]]
+            rating = b_header.get("absent_rating", "bad")
+
+        else:
+            case_s_patterns = b_header.get("case_sensitive_patterns", False)
+
+            if case_s_patterns:
+                v_pattern = re.compile(b_header["validation_pattern"])
+            else:
+                v_pattern = re.compile(b_header["validation_pattern"], re.I)
+
+            if not v_pattern.match(header_value):
+                explanations += [b_header["invalid_explanation"]]
+                rating = b_header.get("invalid_rating", "bad")
+
+            else:
+                rating = b_header.get("default_rating", "bad")
+
+                for r_pattern in b_header.get("rating_patterns", []):
+
+                    if case_s_patterns:
+                        pattern = re.compile(r_pattern["pattern"])
+                    else:
+                        pattern = re.compile(r_pattern["pattern"], re.I)
+
+                    if pattern.match(header_value):
+                        rating = r_pattern["rating"]
+
+                for e_pattern in b_header.get("explanation_patterns", []):
+
+                    if case_s_patterns:
+                        pattern = re.compile(e_pattern["pattern"])
+                    else:
+                        pattern = re.compile(e_pattern["pattern"], re.I)
+
+                    if pattern.match(header_value):
+                        exp = pattern.sub(e_pattern["present"], header_value)
+                        explanations += [exp]
+
+                    elif e_pattern.get("absent") is not None:
+                        explanations += [e_pattern["absent"]]
+
+        if b_header.get("final_explanation") is not None:
+            explanations += [b_header["final_explanation"]]
+
+        findings += [{
+            "header": header_name,
+            "value": header_value,
+            "rating": rating,
+            "explanations": explanations,
+            "references": b_header["references"] if not short else []
+            }]
+
+    return findings
+
 def main():
     parser = ArgumentParser()
 
@@ -164,6 +330,7 @@ def main():
     group.add_argument('-d', '--data',
             help="Data to append to the request."
             " Mutually exclusive with --file")
+
     group.add_argument('-f', '--file', 
             help="Path to a file to append to the request."
             " Mutually exclusive with --data")
@@ -197,6 +364,10 @@ def main():
             help="Optional path to the SSL client .pem certificate"
             " for client authentication")
     
+    parser.add_argument('-b', '--baseline-path',
+            help="Path to the baseline.json file for the header analysis",
+            default="baseline.json")
+    
     parser.add_argument('-a', '--user-agent',
             help="User Agent to use."
             " Defaults to a recent Google Chrome user agent",
@@ -207,6 +378,11 @@ def main():
             help="Shorten the output. Do not print the request parameters,"
             " do not print the response details,"
             " do not print headers' descriptions, do not print references.")
+
+    parser.add_argument('-w', '--max-width', type=int,
+            help="The maximum width of the output. Defaults to the screen"
+            f" width ({get_terminal_size().columns} columns)",
+            default=get_terminal_size().columns)
     
     parser.add_argument('url', help="The url to test")
     
@@ -232,97 +408,28 @@ def main():
 
     if not args.short:
         print_color("\nRequest parameters:", "blue")
-        print(tabulate([[k, "\\\n".join((wrap(str(v), width=WIDTH)))] for k, v in request_arguments.items()]))
+        print(tabulate_dict(request_arguments, args.max_width))
 
     response = requests.request(**request_arguments)
 
     if not args.short:
         print_color("\nResponse:", "blue")
-        print(tabulate([["Length", len(response.content)], ["Status Code", response.status_code], ["Reason", response.reason]]))
+        print(tabulate_dict({
+            "Length": len(response.content),
+            "Status Code": response.status_code,
+            "Reason": response.reason
+            }, args.max_width))
 
     if not args.short:
         print_color("\nResponse headers:", "blue")
-        print(tabulate([[k, "\\\n".join((wrap(str(v), width=WIDTH)))] for k, v in response.headers.items()]))
+        print(tabulate_dict(response.headers, args.max_width))
 
-    findings = []
+    baseline = load_baseline(args.baseline_path)
 
-    with open("baseline.json", "rb") as f:
-        baseline_json = f.read()
-        baseline_json = baseline_json.replace(b"[green]", b"\u001b[92m")
-        baseline_json = baseline_json.replace(b"[yellow]", b"\u001b[93m")
-        baseline_json = baseline_json.replace(b"[red]", b"\u001b[91m")
-        baseline_json = baseline_json.replace(b"[normal]", b"\u001b[0m")
-        baseline = json.loads(baseline_json)
-
-    for header_baseline in baseline["headers"]:
-
-        header_name = header_baseline["name"]
-        header_value = response.headers.get(header_name.lower())
-        explanations = []
-
-        if not args.short and header_baseline.get("description") is not None:
-            explanations += [header_baseline["description"]]
-
-        if header_value is None:
-            explanations += [header_baseline["absent_explanation"]]
-            rating = header_baseline.get("absent_rating", "bad")
-
-        else:
-            case_sentitive_patterns = header_baseline.get("case_sentitive_patterns", False)
-
-            if case_sentitive_patterns:
-                validation_pattern = re.compile(header_baseline["validation_pattern"])
-            else:
-                validation_pattern = re.compile(header_baseline["validation_pattern"], re.IGNORECASE)
-
-            if not validation_pattern.match(header_value):
-                explanations += [header_baseline["invalid_explanation"]]
-                rating = header_baseline.get("invalid_rating", "bad")
-
-            else:
-                rating = header_baseline.get("default_rating", "bad")
-
-                for rating_pattern in header_baseline.get("rating_patterns", []):
-
-                    if case_sentitive_patterns:
-                        pattern = re.compile(rating_pattern["pattern"])
-                    else:
-                        pattern = re.compile(rating_pattern["pattern"], re.IGNORECASE)
-
-                    if pattern.match(header_value):
-                        rating = rating_pattern["rating"]
-
-                for explanation_pattern in header_baseline.get("explanation_patterns", []):
-
-                    if case_sentitive_patterns:
-                        pattern = re.compile(explanation_pattern["pattern"])
-                    else:
-                        pattern = re.compile(explanation_pattern["pattern"], re.IGNORECASE)
-
-                    if pattern.match(header_value):
-                        explanations += [pattern.sub(explanation_pattern["present"], header_value)]
-
-                    elif explanation_pattern.get("absent") is not None:
-                        explanations += [explanation_pattern["absent"]]
-
-        if header_baseline.get("final_explanation") is not None:
-            explanations += [header_baseline["final_explanation"]]
-
-        finding_value = "\\\n".join(wrap(header_value)) if header_value is not None else "Absent"
-        finding_rating = RATINGS.get(rating.lower())
-
-        paragraphs = " ".join(explanations).splitlines()
-        finding_explanation = "\n".join(["\n".join(wrap(p)) for p in paragraphs])
-
-        if not args.short and header_baseline.get("references", []) != []:
-            ref_lines = "\n".join(["\n".join(wrap(r)) for r in header_baseline["references"]]).splitlines()
-            finding_explanation += "\nReferences:\n\033[4;94m" + "\033[0m\n\033[4;94m".join(ref_lines) + "\033[0m"
-
-        findings += [[header_name, finding_value, finding_rating, finding_explanation]]
+    findings = analyse_headers(response.headers, baseline, args.short)
 
     print_color("Header analysis", "blue")
-
-    print(tabulate(findings, headers=["Header", "Value", "Rating", "Explanation"]))
+    print(tabulate_findings(findings, args.max_width))
 
     exit(0)
 
